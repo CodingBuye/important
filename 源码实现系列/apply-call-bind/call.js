ac3862d7962d83834407e48ac5b7cd62c0d6a1d8
wuwenying <wuwenying@bupt.edu.cn>
2020-06-01 10:06:54 +0800

    手写call

---
+++ 源码实现系列/apply-call-bind/call.js
@@ -0,0 +1,28 @@
+var person = {
+	fullName: function(txt, we) {
+		console.log(txt + we + this.firstName + " " + this.lastName);
+
+	}
+}
+
+var person1 = {
+	firstName: 'John',
+	lastName: 'Doe'
+}
+
+person.fullName.call(person1, "Hello, ");
+
+Function.prototype.myCall = function(context) {
+	console.log(this);    // [Function: fullName]
+	console.log(context); // { firstName: 'John', lastName: 'Doe' }
+	
+	var uniqueID = Symbol();
+
+	context = context || window;
+	context[uniqueID] = this;
+	var args = Array.from(arguments).slice(1);
+	var result = context[uniqueID](...args);
+	delete context[uniqueID];
+	return result;
+}
+person.fullName.myCall(person1, "Hello, ", "cdacd");
