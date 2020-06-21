8d4e06e707fe07edf9a140e68bd837f6f0e0adb7
wuwenying <wuwenying@bupt.edu.cn>
2020-06-21 16:37:24 +0800

    手写bind

--- 源码实现系列/apply-call-bind/bind.js
+++ 源码实现系列/apply-call-bind/bind.js
@@ -8,18 +8,27 @@ var objGreeting = greeting.myBind(obj, 'the world');
 
 objGreeting('JS');
 
+
+
 Function.prototype.myBind = function() {
-	console.log(this);
-	console.log(arguments);
-	var thatFunc = this;
-	var thatArg = arguments[0];
-	var args = Array.prototype.slice.call(arguments, 1);
+	var thatFunc = this,  // greeting
+	    thatArg = arguments[0]; // obj
+	var args = Array.prototype.slice.call(arguments, 1); // 'the world'
+
 	if(typeof thatFunc !== 'function') {
-		throw new TypeError("Function.prototype.bind - what is trying to be bound is not callable");
+		throw new TypeError('Function.prototype.bind '+
+			'- what is trying to be bound is not callable');
 	}
 
-	return function() {
-		return thatFunc.apply(thatArg, args);
+	var fBound = function() {
+		return thatFunc.apply(this instanceof fBound ? this : thatArg,
+			args.concat(Array.prototype.slice.call(arguments)))
 	}
+
+	var fNOP = function(){};
+	if(thatFunc.prototype) {
+		fNOP.prototype = thatFunc.prototype;
+	}
+	fBound.prototype = new fNOP();
+	return fBound;
 }
-
