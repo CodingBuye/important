4a12375e3f564da8e63c751c0da7bedc8f818e17
CodingBuye <2515957145@qq.com>
2020-07-24 09:44:38 +0800

    百度-商业推荐部-二面

---
+++ 百度/数字千分位.js
@@ -0,0 +1,26 @@
+/**
+ * 输入数字
+ * 返回一个字符串，千分位
+ * 123456 => 123,456
+ */
+
+var solution = function(num) {
+    // 123456 => "654321"
+    let str = num.toString().split("").reverse().join("");
+    let res ="";
+    for(let i=0,len=str.length;i<len;i++){
+        if((i+1) % 3 === 0 && i!==len-1) {
+            res += (str[i]+",");
+        } else {
+            res += str[i];
+        }
+    }
+
+    res = res.split("").reverse().join("");
+    return res;
+}
+
+// 测试
+console.log(solution(123456));
+console.log(solution(12));
+console.log(solution(1234));
