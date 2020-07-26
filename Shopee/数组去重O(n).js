86fa206866bfaadc5c73b3725041eb83a15b818e
CodingBuye <2515957145@qq.com>
2020-07-26 21:14:43 +0800

    数组去重

---
+++ Shopee/数组去重O(n).js
@@ -0,0 +1,13 @@
+function arrayNoRepeat(arr) {
+    let map = new Map();
+    let res = [];
+    for(let item of arr) {
+        if(!map.has(item)) {
+            res.push(item);
+            map.set(item, true);
+        }
+    }
+    return res;
+}
+
+console.log(arrayNoRepeat([1,2,3,4,5,6,2,3,4,13,434,5,24,23]));
