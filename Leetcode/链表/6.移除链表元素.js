974d1ea0cb7f5d2d2b206981220838eeb86ceaa2
CodingBuye <2515957145@qq.com>
2020-08-02 09:33:13 +0800

    移除链表元素

--- Leetcode/链表/6.移除链表元素.js
+++ Leetcode/链表/6.移除链表元素.js
@@ -1,3 +1,18 @@
 var removeElements = function(head, val) {
-    
+    if(!head) return null;
+    let dummy = new ListNode(-1);
+    dummy.next = head;
+    let prev = dummy;
+    let curr = head;
+    while(curr !== null) {
+        let nx = curr.next;
+        if(curr.val === val) {
+            prev.next = nx;
+            curr = nx;
+        } else {
+            curr = curr.next;
+            prev = prev.next;
+        }
+    }
+    return dummy.next;
 }
