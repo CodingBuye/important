0b0f8bae148bf01230d42bfeae5cfda140a7fa28
CodingBuye <2515957145@qq.com>
2020-08-04 18:40:06 +0800

    leetcode-47-全排列II

--- Leetcode/Local/47.全排列II.js
+++ Leetcode/Local/47.全排列II.js
@@ -1,26 +1,31 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
 var permuteUnique = function(nums) {
     if(nums.length === 0) return [];
     nums.sort((a, b) => a-b);
     let len = nums.length;
-    let res = [];
     let used = new Array(len).fill(false);
+    let res = [];
     let selected = [];
-    dfs(nums, len, used, selected, res);
+    dfs(nums, len, used, res, selected);
     return res;
-}
+};
 
-function dfs(nums, len, used, selected, res) {
+function dfs(nums, len, used, res, selected) {
     if(selected.length === len) {
         res.push([...selected]);
         return;
     }
+
     for(let i=0;i<len;i++){
         if(used[i]) continue;
         if(i > 0 && nums[i] === nums[i-1] && !used[i-1]) continue;
         if(used[i] === false) {
             used[i] = true;
             selected.push(nums[i]);
-            dfs(nums, len, used, selected, res);
+            dfs(nums, len, used, res, selected);
             used[i] = false;
             selected.pop();
         }
