1dd089db5bd628630b56868f4fb9418f64a0b843
CodingBuye <2515957145@qq.com>
2020-08-09 10:25:30 +0800

    leetcode-93

--- Leetcode/Local/IP地址/93.复原IP地址.js
+++ Leetcode/Local/IP地址/93.复原IP地址.js
@@ -1,4 +1,4 @@
-var restoreIPAddresses = function(s) {
+var restoreIpAddress = function(s) {
 	let res = [];
 	let len = s.length;
 	if(len < 4 || len > 12) return res;
@@ -19,7 +19,7 @@ function dfs(s, len, begin, residue, path, res) {
 		if(residue * 3 < len - i) continue;
 		if(check(s, begin, i)) {
 			path.push(s.slice(begin, i+1));
-			dfs(s,len,i+1, residue-1, path, res);
+			dfs(s, len, i+1, residue-1, path, res);
 			path.pop();
 		}
 	}
@@ -27,11 +27,9 @@ function dfs(s, len, begin, residue, path, res) {
 
 function check(s, left, right) {
 	let len = right - left + 1;
-	if(len > 1 && s[left] === '0') {
-		return false;
-	}
+	if(len > 1 && s[0] === "0") return false;
 	let res = Number(s.slice(left, right+1));
-	return res >=0 && res <= 255;
+	return res>=0 && res <= 255;
 }
 
-console.log(restoreIPAddresses("25525511135"));
+console.log(restoreIpAddress("25525511135"))
