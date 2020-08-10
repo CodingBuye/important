b3405f2fb7b9ba686ca8b41dfa89b291f0613c8b
CodingBuye <2515957145@qq.com>
2020-08-10 12:03:03 +0800

    字节

--- 公司题目集锦/字节跳动/阿米诺骨牌.js
+++ 公司题目集锦/字节跳动/阿米诺骨牌.js
@@ -1,3 +1,21 @@
+/**
+ * 题目描述：
+ * 第一行是骨牌个数
+ * 从第二行开始每行代表一个骨牌的高和宽
+ * 
+ * 骨牌的排列要求后一张的宽和高必须大于前一张的宽和高，求最长的骨牌排列
+ * 
+ * 解法：先将数组按照高度进行排序，然后对宽度求最长上升子序列
+ */
+const N = praseInt(readline());
+const arr = [];
+while(N--){
+    let [n, m] = readline().split(" ").map(item => Number(item));
+    arr.push([n,m]);
+}
+print(solve(arr));
+
+
 function solve(arr) {
     arr.sort((a, b) => a[0] - b[0]);
     let maxCount = 0;
