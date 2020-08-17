f6a0b1de204a7c2e4ed2ee946e3dcd79e4e89f02
CodingBuye <2515957145@qq.com>
2020-08-17 19:24:58 +0800

    参考

---
+++ Leetcode/Local/26.删除排序数组中的重复项.js
@@ -0,0 +1,20 @@
+var removeDuplicates = function(nums) {
+    let len = nums.length;
+    if(len <= 1) return len;
+    let left = 0, right = 1;
+    let count = 1;
+    while(right < len) {
+        while(right < len && nums[right] === nums[left]) {
+            right++;
+        }
+        if(right === len) {
+            return count;
+        } else {
+            left += 1;
+            [nums[left], nums[right]] = [nums[right], nums[left]];
+            right += 1;
+            count += 1;
+        }
+    }
+    return count;
+}
