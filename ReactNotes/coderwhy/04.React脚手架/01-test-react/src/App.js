8ba7b69dbd8f632a6a455c555aafd773dac57c13
CodingBuye <2515957145@qq.com>
2020-09-02 15:08:10 +0800

    组件化开发

--- ReactNotes/coderwhy/04.React脚手架/01-test-react/src/App.js
+++ ReactNotes/coderwhy/04.React脚手架/01-test-react/src/App.js
@@ -1,8 +1,51 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
+class HYTestCpn extends Component {
     render() {
-        return <h2>Hello App</h2>
+        return <h2>HYTestCpn</h2>
+    }
+
+    componentWillUnmount() {
+        console.log("HYTestCpn componentWillUnmount");
+    }
+}
+
+export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+        console.log("调用constructor方法");
+    }
+
+    componentDidMount() {
+        console.log("调用componentDidMount方法");
+    }
+    
+    componentDidUpdate() {
+        console.log("调用componentDidUpdate方法");
+    }
+    
+    componentWillUnmount() {
+        console.log("调用componentWillUnmount方法");
+    }
+
+    render() {
+        console.log("调用render方法")
+        return (
+            <div>
+              <h2>当前计数: {this.state.counter}</h2>
+              {this.state.counter <= 5 && <HYTestCpn/>}
+              <button onClick={e => this.increment()}>+1</button>
+            </div>
+        )
+    }
+
+    increment() {
+        this.setState({
+            counter: this.state.counter + 1
+        })
     }
 }
 
