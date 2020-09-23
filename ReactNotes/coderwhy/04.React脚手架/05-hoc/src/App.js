b2fa98f84bb02fc0aca6056e6dcd794ea2dc4087
CodingBuye <2515957145@qq.com>
2020-09-23 18:01:39 +0800

    高阶组件以及组件补充

--- ReactNotes/coderwhy/04.React脚手架/05-hoc/src/App.js
+++ ReactNotes/coderwhy/04.React脚手架/05-hoc/src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import Basic from './1';
-import Header from './2';
+import React, {createRef, forwardRef, PureComponent} from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-      <div>
-        <Basic/>
-        <Header name="aaa" age={18} height={1.60}/>
-      </div>
-  );
+const Home = forwardRef(function(props, ref) {
+    return (
+        <div>
+            <h2 ref={ref}>Home</h2>
+            <button>按钮</button>
+        </div>
+    )
+})
+
+class Modal extends PureComponent {
+
+    render() {
+        return ReactDOM.createPortal(
+            this.props.children,
+            document.getElementById("modal")
+        )
+    }
 }
 
+class App extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.homeTitleRef = createRef();
+    }
+
+    render() {
+        return (
+            <div>
+                <Home ref={this.homeTitleRef}/>
+                <button onClick={e => this.printInfo()}>打印ref</button>
+                <Modal>
+                    <h2>我是标题</h2>
+                </Modal>
+            </div>
+        )
+    }
+
+    printInfo() {
+        console.log(this.homeTitleRef.current);
+    }
+}
 export default App;
