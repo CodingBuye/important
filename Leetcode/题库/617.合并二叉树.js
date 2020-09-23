87ecbfc42311cb772d4232805a8fdab377694838
CodingBuye <2515957145@qq.com>
2020-09-23 09:35:59 +0800

    每日一题

--- Leetcode/题库/617.合并二叉树.js
+++ Leetcode/题库/617.合并二叉树.js
@@ -9,9 +9,8 @@ function TreeNode(val) {
 }
 
 const mergeTrees = function (t1, t2) {
-  if(t1 === null && t2 === null) return null;
-  if(t1 === null || t2 === null) return t1 === null ? t2 : t1;
-  let root = new TreeNode(t1.val + t2.val);
+  if(!t1 || !t2) return t1 ? t1 : t2;
+  let root = new TreeNode(t1.val+t2.val);
   root.left = mergeTrees(t1.left, t2.left);
   root.right = mergeTrees(t1.right, t2.right);
   return root;
