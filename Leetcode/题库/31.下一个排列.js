695cd3851ece177f69f49df8836f43bb10cd4c3d
CodingBuye <2515957145@qq.com>
2020-11-10 12:52:35 +0800

    每日一题

--- Leetcode/题库/31.下一个排列.js
+++ Leetcode/题库/31.下一个排列.js
@@ -1,19 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ * 参考：https://leetcode-cn.com/problems/next-permutation/solution/xia-yi-ge-pai-lie-suan-fa-xiang-jie-si-lu-tui-dao-/
+ */
 var nextPermutation = function(nums) {
+    if(!nums || nums.length <= 1) return;
     let len = nums.length;
-    if(len <= 1) return;
     let i = len-2, j = len-1, k = len-1;
-    while(i>=0 && nums[i] >= nums[j]) {
+    while(i >= 0 && nums[i] >= nums[j]) {
         i-=1;
         j-=1;
     }
 
     if(i >= 0) {
-        while(nums[i] >= nums[k]) {
+        while (nums[i] >= nums[k]) {
             k-=1;
         }
         [nums[i], nums[k]] = [nums[k], nums[i]];
     }
-    for(let i=i, j=len-1;i<j;i++,j--) {
+
+    for(i=j,j=len-1;i<j;i++,j--) {
         [nums[i], nums[j]] = [nums[j], nums[i]];
     }
-}
+};
