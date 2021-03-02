ebabf1bc4e553fcab12ff8b707dbf8a5486fa003
CodingBuye <2515957145@qq.com>
2021-03-02 15:35:53 +0800

    删除ES6

--- ES6/01.let与const.js
+++
@@ -1,7 +0,0 @@
-{
-    let a = 10;
-    let b = 1;
-}
-
-console.log(a); // a is not defined
-console.log(b); // 1
--- ES6/ReadMe.md
+++
@@ -1,3 +0,0 @@
-# 参考
-
-- ES6 入门教程：[https://es6.ruanyifeng.com/](https://es6.ruanyifeng.com/)
