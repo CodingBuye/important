b2e4f2312e4b513be886102a7fbad1fc586a27ab
CodingBuye <2515957145@qq.com>
2021-03-04 10:21:16 +0800

    第354题

--- Leetcode/2021-03/354.俄罗斯套娃信封问题.js
+++ Leetcode/2021-03/354.俄罗斯套娃信封问题.js
@@ -17,11 +17,30 @@ var maxEnvelopes = function(envelopes) {
     for(let i=0;i<len;i++){
         height[i] = envelopes[i][1];
     }
-    return envelopes;
+    return lengthOfLIS(height);
 };
 
 function lengthOfLIS(arr) {
-    
+    let len = arr.length;
+    let top = new Array(len);
+    let piles = 0;
+    for(let i=0;i<len;i++) {
+        let poker = arr[i];
+        let left = 0, right = piles;
+        while(left < right) {
+            let mid = parseInt((left+right)/2);
+            if(top[mid] > poker) {
+                right = mid;
+            } else if(top[mid] < poker) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        if(left === piles) piles+=1;
+        top[left] = poker;
+    }
+    return piles;
 }
 
 console.log(maxEnvelopes([[5,4],[6,4],[6,7],[2,3]]));
