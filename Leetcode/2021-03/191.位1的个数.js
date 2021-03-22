df5b338554ee5d7d506d59167c88e7565a1e2eec
wuwenying <2515957145@qq.com>
2021-03-22 11:41:19 +0800

    每日一题：第191题

---
+++ Leetcode/2021-03/191.位1的个数.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function(n) {
+    let count = 0;
+    let mask = 1;
+    for(let i=0;i<32;i++){
+        if((n & mask) !== 0) {
+            count++;
+        }
+        mask <<= 1;
+    }
+    return count;
+};
