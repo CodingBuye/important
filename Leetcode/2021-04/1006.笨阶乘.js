bf6e60dc55516f696eacd2064145b770e841d5c2
wuwenying <2515957145@qq.com>
2021-04-01 23:43:28 +0800

    每日一题：第1006题

---
+++ Leetcode/2021-04/1006.笨阶乘.js
@@ -0,0 +1,29 @@
+const clumsy = function (N) {
+    const stack = [N--];
+
+    let index = 0; // 用于控制乘、除、加、减
+    while (N > 0) {
+        if (index % 4 === 0) {
+            stack.push(stack.pop() * N);
+        } else if (index % 4 === 1) {
+            const cur = stack.pop();
+            stack.push(cur > 0 ? Math.floor(cur / N) : Math.ceil(cur / N));
+        } else if (index % 4 === 2) {
+            stack.push(N);
+        } else {
+            stack.push(-N);
+        }
+        index++;
+        N--;
+    }
+
+    // 把栈中所有的数字依次弹出求和
+    let sum = 0;
+    stack.forEach((element) => {
+        sum += element;
+    })
+    return sum;
+};
+
+console.log(clumsy(10));
+console.log(clumsy(4));
